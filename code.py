1ddb14315b3aa1c4106843ee55b26c885396f5f5
Balram <webdevforindia@gmail.com>
2021-08-11 23:56:04 +0530

    sdnk

---
+++ code.py
@@ -0,0 +1,20 @@
+# import holidays
+import datetime
+
+# for day in sorted(holidays.India(years=2021, state='MH').items()):
+#     print(day)
+
+# india = holidays.India(years=2021)
+
+# print("2021, 8,15" in india)
+import time
+
+# curr_month = datetime.datetime.now().strftime("%d")
+# print(curr_month)
+
+# def clock():
+#     while True:
+#         print(datetime.datetime.now().strftime("%H:%M:%S"), end="\r")
+#         time.sleep(1)
+
+# clock()
--- mainDef.py
+++ mainDef.py
@@ -1,18 +1,28 @@
 from datetime import date
-# import json
 import time
 import server
+import datetime
+import personsData
+import json
+
+file = json.loads(personsData.Person)
+
+curr_month = datetime.datetime.now().strftime("%B")
+curr_date = datetime.datetime.now().strftime("%d")
 
 
+def main_Func():
+    for i in file:
+        if  curr_month in i["bdate"]:
+            if curr_date in i["bdate"]:
+                server.server.sendmail("potabattiram@gmail.com",i["emailId"],"Hello, "+i["name"]," Wish you a happy birthday bro")
+                print("Check your Email Once!")
 
-# starttime = time.time()
-# interval = 20
 
-# def main_Func():
-#     server.server.sendmail("potabattiram@gmail.com","potabattiram@gmail.com","potabattiram@gmail, Hello")
-#     print("Check your Email Once!")
+starttime = time.time()
+interval = 3600
+while True:
+    main_Func()
+    time.sleep(interval - ((time.time() - starttime) % interval))
+    
 
-
-# while True:
-#     main_Func()
-#     time.sleep(interval - ((time.time() - starttime) % interval))
