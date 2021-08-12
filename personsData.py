aad304889d378ed83fc47f7910c4ef7880c5dad2
Balram <webdevforindia@gmail.com>
2021-08-12 21:58:39 +0530

    dffd

--- personsData.py
+++ personsData.py
@@ -3,8 +3,20 @@ Person = '''[
     {
         "name":"Balram Potabatti",
         "b_month":"August",
-        "b_date":"12",
+        "b_date":"13",
         "emailId":"potabattiram@gmail.com"
+    },
+    {
+        "name":"Shrihari Potabatti",
+        "b_month":"August",
+        "b_date":"13",
+        "emailId":"shriharipotabatti67@gmail.com"
+    },
+    {
+        "name":"Geeta Potabatti",
+        "b_month":"August",
+        "b_date":"14",
+        "emailId":"geetapotabatti277@gmail.com"
     }
 ]'''
 
