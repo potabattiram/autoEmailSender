3b1dbefaf5cb3e5e524f0b579280a5439f53d90d
Balram <webdevforindia@gmail.com>
2021-08-15 12:17:14 +0530

    sdds

--- Clock.py
+++ Clock.py
@@ -7,16 +7,19 @@ server = smtplib.SMTP_SSL("smtp.gmail.com",465)
 server.login("potabattiram@gmail.com","Potabatti9012@")
 
 def At_12():
-    server.sendmail("potabattiram@gmail.com","potabattiram@gmail.com",'Its 10')
-    print("12:00 Testing")
+    server.sendmail("potabattiram@gmail.com","potabattiram@gmail.com",'Testing Phase')
+    print("Its Testing Phase Bro.")
  
 def Test():
      print('Test')
 
 schedule.every().seconds.do(Test)
+schedule.every().day.at("07:00").do(At_12)
+schedule.every().day.at("10:00").do(At_12)
+schedule.every().day.at("12:00").do(At_12)
 schedule.every().day.at("18:30").do(Mail_Sender)
 
 while True:
     schedule.run_pending()
-    time.sleep(50)
+    time.sleep(59)
 
