a9f711962c2af1937b65806579e65ec8bc7fc77a
Balram <webdevforindia@gmail.com>
2021-08-15 16:14:40 +0530

    sdsdgtg

--- Clock.py
+++ Clock.py
@@ -15,7 +15,7 @@ def Test():
 
 schedule.every().seconds.do(Test)
 
-schedule.every().day.at("16:12").do(At_12)
+schedule.every().day.at("10:50").do(At_12)
 schedule.every().day.at("07:00").do(At_12)
 schedule.every().day.at("10:00").do(At_12)
 schedule.every().day.at("12:00").do(At_12)
