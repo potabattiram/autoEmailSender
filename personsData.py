248a6bf8105609422e8dd6ab6357bf3951d2816c
Balram <webdevforindia@gmail.com>
2021-08-15 22:09:17 +0530

    sdsdd

--- personsData.py
+++ personsData.py
@@ -3,7 +3,7 @@ Person = '''[
     {
         "name":"Balram Potabatti",
         "b_month":"August",
-        "b_date":"15",
+        "b_date":"16",
         "emailId":"potabattiram@gmail.com"
     },
     {
