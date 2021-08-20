ee3b8ca9f824625badcf5f293e04348610a42a73
Balram <webdevforindia@gmail.com>
2021-08-20 17:48:51 +0530

    sdds

--- personsData.py
+++ personsData.py
@@ -3,19 +3,19 @@ Person = '''[
     {
         "name":"Balram Potabatti",
         "b_month":"August",
-        "b_date":"19",
+        "b_date":"21",
         "emailId":"potabattiram@gmail.com"
     },
     {
         "name":"Ram Potabatti",
         "b_month":"August",
-        "b_date":"20",
+        "b_date":"22",
         "emailId":"webdevforindia@gmail.com"
     },
     {
         "name":"Shrihari Potabatti",
         "b_month":"August",
-        "b_date":"21",
+        "b_date":"23",
         "emailId":"shriharipotabatti67@gmail.com"
     }
 ]'''
