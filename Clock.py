61b266e4dc10de27fea9cd80bfc935c1222501c3
Balram <webdevforindia@gmail.com>
2021-08-21 16:18:22 +0530

    sdds

--- Clock.py
+++ Clock.py
@@ -15,12 +15,9 @@ def Test():
 
 schedule.every().seconds.do(Test)
 
-schedule.every().day.at("04:00").do(At_12)
-schedule.every().day.at("05:00").do(At_12)
-schedule.every().day.at("06:00").do(At_12)
-schedule.every().day.at("07:00").do(At_12)
-schedule.every().day.at("08:00").do(At_12)
-schedule.every().day.at("10:10").do(At_12)
+schedule.every().day.at("10:56").do(At_12)
+schedule.every().day.at("11:46").do(At_12)
+schedule.every().day.at("12:46").do(At_12)
 
 schedule.every().day.at("18:30").do(Mail_Sender)
 
