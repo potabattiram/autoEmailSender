68f9aca2470683b0f6ae59340cc303bfea58ac3e
Balram <webdevforindia@gmail.com>
2021-11-04 15:24:42 +0530

    V1.1.0

--- personsData.py
+++ personsData.py
@@ -11,12 +11,6 @@ Person = '''[
         "b_month":"August",
         "b_date":"23",
         "emailId":"webdevforindia@gmail.com"
-    },
-    {
-        "name":"Shrihari Potabatti",
-        "b_month":"August",
-        "b_date":"23",
-        "emailId":"shriharipotabatti67@gmail.com"
     }
 ]'''
 
